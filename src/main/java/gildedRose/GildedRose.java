2a9a06d712966901d4aad7eb1b719f747b1febb7
GUOED3 <EDDY.GUO@OOCL.COM>
2019-08-19 22:16:29 +0800

    refactor:extract updateQuality method to item class

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,63 +12,7 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            switch (items[i].name) {
-                case "Aged Brie":
-                    updateByNameAged(items[i]);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    updateByNameBackstage(items[i]);
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    updateByNameSulfuras(items[i]);
-                    break;
-                default:
-                    updateByOtherName(items[i]);
-            }
-
-        }
-    }
-
-    private void updateByOtherName(Item item) {
-        if (item.quality > 0) {
-            item.quality--;
-        }
-        item.sellIn--;
-        if (item.sellIn < 0 && item.quality > 0) {
-            item.quality--;
-        }
-    }
-
-    private void updateByNameAged(Item item) {
-        increaseIfLessThanBaseQuality(item);
-        item.sellIn--;
-        if (item.sellIn < 0 && item.quality < BASE_QUALITY) {
-            item.quality++;
-        }
-    }
-
-    private void updateByNameBackstage(Item item) {
-        if (item.quality < BASE_QUALITY) {
-            item.quality++;
-            if (item.sellIn < HIGHER_SELLIN && item.quality < BASE_QUALITY) {
-                item.quality++;
-            }
-            if (item.sellIn < FEWER_SELLIN) {
-                increaseIfLessThanBaseQuality(item);
-            }
-        }
-        item.sellIn--;
-        if(item.sellIn < 0){
-            item.quality = 0;
-        }
-    }
-
-    private void updateByNameSulfuras(Item item) {
-    }
-
-    private void increaseIfLessThanBaseQuality(Item item) {
-        if (item.quality < BASE_QUALITY) {
-            item.quality++;
+            items[i].updateQuality();
         }
     }
 
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -1,5 +1,9 @@
 package gildedRose;
 
+import gildedRose.updator.AgedRoseUpdator;
+
+import static gildedRose.GildedRose.*;
+
 public class Item {
 
     public String name;
@@ -12,6 +16,58 @@ public class Item {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+    }
+
+    public void updateQuality() {
+        switch (this.name) {
+            case "Aged Brie":
+                AgedRoseUpdator agedRoseUpdator = new AgedRoseUpdator();
+                agedRoseUpdator.updateQuality(this);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                updateByNameBackstage(this);
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                updateByNameSulfuras(this);
+                break;
+            default:
+                updateByOtherName(this);
+        }
+    }
+
+    private void updateByOtherName(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void updateByNameBackstage(Item item) {
+        if (item.quality < BASE_QUALITY) {
+            item.quality++;
+            if (item.sellIn < HIGHER_SELLIN && item.quality < BASE_QUALITY) {
+                item.quality++;
+            }
+            if (item.sellIn < FEWER_SELLIN) {
+                increaseIfLessThanBaseQuality(item);
+            }
+        }
+        item.sellIn--;
+        if(item.sellIn < 0){
+            item.quality = 0;
+        }
+    }
+
+    private void updateByNameSulfuras(Item item) {
+    }
+
+    private void increaseIfLessThanBaseQuality(Item item) {
+        if (item.quality < BASE_QUALITY) {
+            item.quality++;
+        }
     }
 
    @Override
