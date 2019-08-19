c4e4b105cec1cc8e088b1baae7b6bca16087f499
GUOED3 <EDDY.GUO@OOCL.COM>
2019-08-19 22:24:11 +0800

    refactor: extract updateBySulfuras method to class SulfurasRoseUpdator

---
+++ src/main/java/gildedRose/updator/BackstageRoseUpdator.java
@@ -0,0 +1,30 @@
+package gildedRose.updator;
+
+import gildedRose.Item;
+
+import static gildedRose.GildedRose.*;
+
+public class BackstageRoseUpdator {
+
+    public void updateQuality(Item item) {
+        if (item.quality < BASE_QUALITY) {
+            item.quality++;
+            if (item.sellIn < HIGHER_SELLIN && item.quality < BASE_QUALITY) {
+                item.quality++;
+            }
+            if (item.sellIn < FEWER_SELLIN) {
+                increaseIfLessThanBaseQuality(item);
+            }
+        }
+        item.sellIn--;
+        if(item.sellIn < 0){
+            item.quality = 0;
+        }
+    }
+
+    private void increaseIfLessThanBaseQuality(Item item) {
+        if (item.quality < BASE_QUALITY) {
+            item.quality++;
+        }
+    }
+}
---
+++ src/main/java/gildedRose/updator/SulfurasRoseUpdator.java
@@ -0,0 +1,9 @@
+package gildedRose.updator;
+
+import gildedRose.Item;
+
+public class SulfurasRoseUpdator {
+
+    public void updateQuality(Item item) {
+    }
+}
